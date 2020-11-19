57e08e887cf2b827689f7ffa4d01f11b5625f9a3
abarna-dot <abarna.m04@gmail.com>
2020-11-19 15:13:56 +0530

    second commit

--- connection.php
+++ connection.php
@@ -1,11 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "test";
+//Development Connection
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$database = "test";
+
+//Remote Daatabase
+$servername = "remotemysql.com";
+$username = "MCaYD87EH7";
+$password = "9NvFpmggl3";
+$database = "MCaYD87EH7";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password,$database);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
 if (!$conn) {
